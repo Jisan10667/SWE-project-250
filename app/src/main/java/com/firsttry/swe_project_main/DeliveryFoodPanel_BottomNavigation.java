5eae95a3aaefcb60b14e2e1817cc6bfc65a254ef
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-02-05 22:38:10 +0600

    firebase recovery & others

---
+++ app/src/main/java/com/firsttry/swe_project_main/DeliveryFoodPanel_BottomNavigation.java
@@ -0,0 +1,50 @@
+package com.firsttry.swe_project_main;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.xyz.demo_4.deliveryFoodPanel.DeliveryPendingOrderFragment;
+import com.xyz.demo_4.deliveryFoodPanel.DeliveryShipOrderFragment;
+
+
+public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_food_panel__bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.delivery_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.shiporders:
+                fragment=new DeliveryShipOrderFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.pendingorders:
+                fragment=new DeliveryPendingOrderFragment();
+                break;
+        }
+        return loaddeliveryfragment(fragment);
+    }
+
+    private boolean loaddeliveryfragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+}
