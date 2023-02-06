56d28a42d73f67207bd1235f84263d0723d0d374
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-02-06 17:04:44 +0600

    all registration work done!

---
+++ app/src/main/java/com/firsttry/swe_project_main/ForgotPassword.java
@@ -0,0 +1,14 @@
+package com.firsttry.swe_project_main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
